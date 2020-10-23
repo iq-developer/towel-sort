6431425f7800adb68bf3adbe9788dd36b88f6a71
Vladimir Sevastianov <108.nimay@gmail.com>
2020-10-23 11:46:57 +0300

    init: Towel Sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
+//module.exports = function towelSort (matrix) {
+function towelSort (matrix) {
   return [];
 }
+
+console.log( towelSort( [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+ ] ) );
